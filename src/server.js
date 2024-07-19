b25927d7ce01ca3aa97887304c4be577d01bd4df
Samson Adewumi <samopzz@gmail.com>
2024-07-19 04:05:06 +0100

    added third party auth and fixed some bugs

--- src/server.js
+++ src/server.js
@@ -2,8 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import colors from "colors";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
+import pkg from "express-openid-connect";
+import helmet from "helmet"; 
 import { ENVIRONMENT } from "./common/config/environment.js";
 import { connectDB } from "./common/config/database.js";
+
+
+import auth0Middleware from "../auth/auth0.js";
+
 
 // Default app configurations
 const app = express();
@@ -15,6 +22,21 @@ const appName = ENVIRONMENT.APP.NAME;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+// Auth0 middleware for authentication and authorization of requests to the API endpoints 
+app.use(auth0Middleware);
+const { requiresAuth } = pkg;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+
+app.use(helmet()); // Set security headers
 
 (async () => {
   try {
@@ -37,7 +59,7 @@ app.use(morgan("dev"));
 (async () => {
   try {
     const authRoutes = await import("./common/routes/authRoutes.js");
-    app.use("/api/v1/auth", authRoutes.default);
+    app.use("/api/v1/auth", requiresAuth(), authRoutes.default);
   } catch (error) {
     console.error("Failed to load user routes:", error);
   }
